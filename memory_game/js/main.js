c61c0ff35f86d1128adaff34e681a2da02fdbf28
Nick Ortiz <nickortiz@Nicks-MacBook-Pro.local>
2019-06-21 13:16:08 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,38 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamons.png:"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  },
+];
 let cardsInPlay = [];
 function checkForMatch() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === [cardsInPlay[1]]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
   }
 }
 function flipCard(cardId) {
-  console.log("User flipped " + cards[cardID]);
-  cardsInPlay.push(cards[cardID]);
+  console.log("User flipped " + cards[cardId].rank + "  " + cards[cardId].cardImage + "  " + cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 }
 flipCard(0);
