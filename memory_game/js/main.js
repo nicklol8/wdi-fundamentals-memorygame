afa2e4c3dae86e4a29ba0a5cf24c7f15cbca37d7
Nick Ortiz <nickortiz@Nicks-MacBook-Pro.local>
2019-06-21 11:26:26 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,16 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
-} else if (cardsInPlay.length === 2 && cardsInPlay[0] != cardsInPlay[1]) {
-  alert("Sorry, try again.");
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
 }
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardID]);
+  cardsInPlay.push(cards[cardID]);
+  checkForMatch();
+}
+flipCard(0);
+flipCard(2);
